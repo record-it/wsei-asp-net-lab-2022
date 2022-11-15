05f4cc89040ddfef39e6d81a0e4776d87c288acd
record-it <67925453+record-it@users.noreply.github.com>
2022-11-15 21:32:16 +0100

    Delete lab-4 directory

--- lab-4/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace wsei_asp_net_lab.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
