fae263ad9b13f3a86dfedd4591bbcadc75b101e0
cs <siwon.cezary@gmail.com>
2022-11-15 21:03:15 +0100

    init

---
+++ lab-5/Migrations/20221115102247_Initial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace lab5.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Created", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 11, 15, 11, 22, 47, 165, DateTimeKind.Local).AddTicks(5875), new DateTime(2022, 2, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "ASP.NET 6.0.0" },
+                    { 2, new DateTime(2022, 11, 15, 11, 22, 47, 165, DateTimeKind.Local).AddTicks(5908), new DateTime(2022, 2, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "C# 10.0" },
+                    { 3, new DateTime(2022, 11, 15, 11, 22, 47, 165, DateTimeKind.Local).AddTicks(5913), new DateTime(2021, 12, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), "Java 19" },
+                    { 4, new DateTime(2022, 11, 15, 11, 22, 47, 165, DateTimeKind.Local).AddTicks(5917), new DateTime(2022, 8, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "JavaScript" },
+                    { 5, new DateTime(2022, 11, 15, 11, 22, 47, 165, DateTimeKind.Local).AddTicks(5922), new DateTime(2019, 10, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Node.js" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ lab-5/Migrations/20221115132550_aa.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab5.Migrations
+{
+    /// <inheritdoc />
+    public partial class aa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 50, 3, DateTimeKind.Local).AddTicks(6113));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 50, 3, DateTimeKind.Local).AddTicks(6155));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 50, 3, DateTimeKind.Local).AddTicks(6159));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 50, 3, DateTimeKind.Local).AddTicks(6163));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 50, 3, DateTimeKind.Local).AddTicks(6167));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 5, 45, DateTimeKind.Local).AddTicks(7571));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 5, 45, DateTimeKind.Local).AddTicks(7603));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 5, 45, DateTimeKind.Local).AddTicks(7607));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 5, 45, DateTimeKind.Local).AddTicks(7611));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Created",
+                value: new DateTime(2022, 11, 15, 14, 25, 5, 45, DateTimeKind.Local).AddTicks(7615));
+        }
+    }
+}
